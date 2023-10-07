deebf000a5cbd4fbfb1eb4497264119764b18e2a
Quer4ikkk <dlyatlauncher@gmail.com>
2023-10-07 19:31:36 +0300

    pup

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*int a=5;
+        double b = 1.7;
+        float c = 1.7f;
+        long d = 5L;
+        System.out.println(a);
+        char a1 = 'p';
+        //String name = "Ivan";
+
+        int i = 5+7; // 13123 :) .toString();
+        int f1 = 5;
+        int f2 = 8;
+        double l1 = (double) f1/f2;
+        System.out.println(i);
+        System.out.println((int)l1);
+        double e1 = 5.7;
+        double e2 = 6.3;
+        System.out.println(e1 * e2);
+
+        String result = "nigga" + "world";
+        System.out.println(result);
+
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        System.out.println("Age = "+age);
+        scanner.nextLine();
+        String name = scanner.nextLine();
+        System.out.println("Name = "+name);
+
+        double x2 =Math.pow(age,(double) 1/3);
+        System.out.println("Квадрат = "+x2);
+
+        double sqrt = Math.sqrt(121);
+        System.out.println(sqrt);
+        boolean condition = age!=18;
+
+        if (!condition) { //age == 18;
+            System.out.println("Всё ок, покупай подики");
+        }
+        else {
+            System.out.println("Иди учись, школьник");
+        }
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        String name1 = "Ivan";
+
+        int age = 19;
+
+        if (name1.equals("Ivan") || age>20)
+            System.out.println("Всё норм");
+    }
+}
